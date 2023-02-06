6732db21e14e1a8dc5a25f30c1e091f224b94478
Zarif <zarifraja19@gmail.com>
2023-02-06 11:59:14 +0600

    added

--- Problem4/index.js
+++ Problem4/index.js
@@ -1,15 +1,23 @@
 
 function publicBusFare(humanNumber){
 
-    let resthuman;
+    let resthuman1;
+    let resthuman2;
 
-  
-    if (humanNumber % 50 === 0 || humanNumber % 11 === humanNumber % 50) {
-      return 0;
-    } 
+  if(humanNumber % 50 !==0 || (humanNumber % 50) > 11 ){
+resthuman1 = humanNumber % 50;
+resthuman2 = resthuman1 % 11;
+return resthuman2*250;
+  }
+    else if(humanNumber % 50 !==0 || humanNumber % 50 < 11){
+      resthuman1 = humanNumber % 50;
+      return resthuman1*250;
+    }
     
-   
+   else{
+    return 0;
+   }
 
 }
 
-console.log(publicBusFare(61));
+console.log(publicBusFare(365));
